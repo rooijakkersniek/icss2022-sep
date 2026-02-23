92ef02576127615abd432dfb06f9422bd5ec16ce
nierooij <niek.rooijakkers@avisi.nl>
2026-02-23 11:32:08 +0100

    Check types in Operations (CH02, CH03)

--- startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
+++ startcode/src/main/java/nl/han/ica/icss/checker/Checker.java
@@ -3,6 +3,7 @@ package nl.han.ica.icss.checker;
 import nl.han.ica.icss.ast.AST;
 import nl.han.ica.icss.ast.ASTNode;
 import nl.han.ica.icss.ast.Expression;
+import nl.han.ica.icss.ast.Operation;
 import nl.han.ica.icss.ast.VariableAssignment;
 import nl.han.ica.icss.ast.VariableReference;
 import nl.han.ica.icss.ast.literals.BoolLiteral;
@@ -10,6 +11,9 @@ import nl.han.ica.icss.ast.literals.ColorLiteral;
 import nl.han.ica.icss.ast.literals.PercentageLiteral;
 import nl.han.ica.icss.ast.literals.PixelLiteral;
 import nl.han.ica.icss.ast.literals.ScalarLiteral;
+import nl.han.ica.icss.ast.operations.AddOperation;
+import nl.han.ica.icss.ast.operations.MultiplyOperation;
+import nl.han.ica.icss.ast.operations.SubtractOperation;
 import nl.han.ica.icss.ast.types.ExpressionType;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -34,15 +38,19 @@ public class Checker {
         if(node instanceof VariableReference) {
             checkForExistenceVariableReference((VariableReference) node);
         }
+
+        if(node instanceof Operation){
+            checkOperations((Operation) node);
+        }
     }
 
-    public void addVariableAssignment(VariableAssignment variableAssignment) {
+    private void addVariableAssignment(VariableAssignment variableAssignment) {
         String name = variableAssignment.name.name;
         ExpressionType type = getExpressionType(variableAssignment.expression);
         variableTypes.get(0).put(name, type);
     }
 
-    public void checkForExistenceVariableReference(VariableReference variableReference) {
+    private void checkForExistenceVariableReference(VariableReference variableReference) {
         boolean found = false;
 
         for (HashMap<String, ExpressionType> variableType : variableTypes) {
@@ -55,6 +63,38 @@ public class Checker {
         if(!found){
             variableReference.setError("Variable " + variableReference.name + " not found");
         }
+    }
+
+    private void checkOperations(Operation node) {
+        ExpressionType left = getExpressionType(node.lhs);
+        ExpressionType right = getExpressionType(node.rhs);
+
+        if(checkForColorExpressionType(left, right)){
+            node.setError("No color allowed in operation");
+        }
+
+        if(node instanceof AddOperation || node instanceof SubtractOperation){
+            if(!checkForMatchingOperationExpression(left, right)){
+                node.setError("Expression Type Mismatch");
+            }
+        }
+        if(node instanceof MultiplyOperation){
+            if(checkForScalar(left, right)){
+                node.setError("No Scalar in MultiplyOperation");
+            }
+        }
+    }
+
+    private boolean checkForColorExpressionType(ExpressionType left, ExpressionType right) {
+        return left == ExpressionType.COLOR || right == ExpressionType.COLOR;
+    }
+
+    private boolean checkForScalar(ExpressionType left, ExpressionType right) {
+        return left != ExpressionType.SCALAR && right != ExpressionType.SCALAR;
+    }
+
+    private boolean checkForMatchingOperationExpression(ExpressionType left, ExpressionType right) {
+        return left == right;
     }
 
     private ExpressionType getExpressionType(Expression expression) {
